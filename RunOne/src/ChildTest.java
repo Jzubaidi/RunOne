53dc961e7b952586d9cf4e6f0e89c5cf2c3f6c2d
jeffzuo <jeffzuo@10.0.0.110>
2022-08-23 14:00:15 -0700

    AddChild

---
+++ RunOne/src/ChildTest.java
@@ -0,0 +1,10 @@
+
+public class ChildTest extends TestOne {
+
+	public int Add(int x,int y) {
+		int z =x*y;
+		System.out.print("the result ="+ z);
+		return z;
+	}
+
+}
--- RunOne/src/TestOne.java
+++ RunOne/src/TestOne.java
@@ -2,8 +2,11 @@
 public class TestOne {
 
 	public static void main(String[] args) {
-		System.out.print("Thank you to visit my GitHub");
-
+		System.out.println("Thank you to visit my GitHub");
+		ChildTest childtest = new ChildTest();
+		childtest.Add(5,5);
 	}
+
+
 
 }
